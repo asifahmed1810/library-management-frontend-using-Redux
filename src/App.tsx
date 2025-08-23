9d34460996845e64895269c2ec20bf1f1285ff42
asifahmed1810 <asifahmed18oct@gmail.com>
2025-08-23 16:05:53 +0600

    Added navbar.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { Button } from "./components/ui/button"
 
 
-import './App.css'
+
 
 function App() {
  
   return (
     <>
      
-      <h1>Vite + React</h1>
+       <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button className="bg-amber-700">Click me</Button>
+    </div>
       
     </>
   )
